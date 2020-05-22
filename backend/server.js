168995ea1adb062df4a57588312d30b8b5608164
jjyoon7 <jay.yoon7@gmail.com>
2020-05-22 21:12:08 +0200

    backend server set up

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = 5000 || process.env.PORT
+
+const userRoutes = require('./routes/user')
+const uri = process.env.ATLAS_URI
+
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+app.use('/user', userRoutes)
+
+mongoose.connect(uri,  { 
+                        useNewUrlParser: true,
+                        useCreateIndex: true,
+                        useUnifiedTopology: true
+                        })
+        
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('mongoDB database connection established successfully.')
+})
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+
+
+
