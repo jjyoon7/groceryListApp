27b2ed50e7892949eac995433cf29807fa8f488b
jjyoon7 <jay.yoon7@gmail.com>
2020-05-23 18:11:22 +0200

    uinsg .find() instead of .findOne

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,17 +3,17 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-
+ 
 const User = require('../models/user.model')
 
-//sign up, create new user if user input email does not exist in database
 router.post('/signup', (req, res, next) => {
-    User.findOne({email: req.body.email})
+    User.find({email: req.body.email})
         .exec()
         .then(user => {
-            if(user.email === req.body.email){
+            //422 unprocessable entity
+            if (user.length >= 1) {
                 return res.status(422).json({
-                    message: 'this email address already exists.'
+                    message: 'this email address already exist'
                 })
             } else {
                 bcrypt.hash(req.body.password, 10, (err, hash) => {
@@ -21,13 +21,14 @@ router.post('/signup', (req, res, next) => {
                         return res.status(500).json({
                             error: err
                         })
-                    } else {
+                    } else { 
                         const user = new User({
                             _id: new mongoose.Types.ObjectId(),
                             email: req.body.email,
                             password: hash
                         })
-                        user.save()
+                        user
+                            .save()
                             .then(result => {
                                 console.log(result)
                                 res.status(201).json({
@@ -46,49 +47,59 @@ router.post('/signup', (req, res, next) => {
         })
 })
 
-//login post request, find the user with email input
 router.post('/login', ( req, res, next ) => {
-    User.findOne({email: req.body.email})
+    User.find({ email: req.body.email })
         .exec()
         .then(user => {
-            if(user.email !== req.body.email) {
-                //404 no user found is too specific information
-                //where hackers can try different emails to check which email exist
-                //in the database or not. so staying vague with 401
+            //find the user with email address
+            if(user.length < 1){
+                //reason why not using 404 and sending 'no user found' is 
+                //bcs hackers can use this to find which email exist and not
                 return res.status(401).json({
                     message: 'Authorization failed'
                 })
             }
-            //compare if the pw user typed in is matching
-            //with the email from the database
-            bcrypt.compare(req.body.password, user.password, (err, result) => {
+            //compare if the paswword which user typed in is matching with user.password in database
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
                 if(err) {
                     return res.status(401).json({
                         message: 'Authorization failed'
                     })
                 }
-                //when compare sucess, create a token and return it with the sucess msg
                 if(result) {
                     const token = jwt.sign(
-                        {
-                            email: user.email,
-                            userId: user._id
-                        },
-                        process.env.JWT_KEY,
-                        {
-                            expiresIn: '1h'
-                        }
+                     {
+                        email: user[0].email,
+                        userId: user[0]._id
+                     }, 
+                     process.env.JWT_KEY, 
+                     {
+                         expiresIn:'1h'
+                     }
                     )
                     return res.status(200).json({
-                        message: 'Authorization sucess',
+                        message: 'Authorization success',
                         token
                     })
                 }
-
+                //when user tries to log in w no-existing email and pw it will fail
+                return res.status(401).json({
+                    message: 'Authorization failed'
+                })
             })
         })
-        //when user tries to log in w non-existing email and pw
-        //it will throw an error
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+router.delete('/:userId', (req, res, next) => {
+    User.remove({_id: req.params.userId})
+        .exec()
+        .then(result => result.status(200).json({ message: 'User deleted'}))
         .catch(err => {
             console.log(err)
             res.status(500).json({
