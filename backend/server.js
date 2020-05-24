c4735f76018f2b488abccdffb54afcae2aa8ec25
jjyoon7 <jay.yoon7@gmail.com>
2020-05-24 20:25:42 +0200

    add grocery routes to erver

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,13 @@ const PORT = 5000 || process.env.PORT
 require('dotenv').config()
 
 const userRoutes = require('./routes/user')
+const groceryRoutes = require('./routes/grocery')
 const uri = process.env.ATLAS_URI
 
 app.use(cors())
 app.use(express.json())
 app.use('/user', userRoutes)
+app.use('/grocery', groceryRoutes)
 
 mongoose.connect(uri,  { 
                         useNewUrlParser: true,
