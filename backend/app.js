67bd75dfd109f356b80d0aee3ab94aa4ef4f9fcf
jjyoon7 <jay.yoon7@gmail.com>
2020-05-24 21:50:51 +0200

    separate server and app.js

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+
+require('dotenv').config()
+
+const userRoutes = require('./routes/user')
+const groceryRoutes = require('./routes/grocery')
+const uri = process.env.ATLAS_URI
+
+app.use(cors())
+app.use(express.json())
+app.use('/user', userRoutes)
+app.use('/grocery', groceryRoutes)
+
+mongoose.connect(uri,  { 
+                        useNewUrlParser: true,
+                        useCreateIndex: true,
+                        useUnifiedTopology: true
+                        })
+        
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('mongoDB database connection established successfully.')
+})
+
+module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,9 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const app = express()
+const http = require('http')
 const PORT = 5000 || process.env.PORT
+const app = require('./app')
+const server = http.createServer(app)
 
-require('dotenv').config()
-
-const userRoutes = require('./routes/user')
-const groceryRoutes = require('./routes/grocery')
-const uri = process.env.ATLAS_URI
-
-app.use(cors())
-app.use(express.json())
-app.use('/user', userRoutes)
-app.use('/grocery', groceryRoutes)
-
-mongoose.connect(uri,  { 
-                        useNewUrlParser: true,
-                        useCreateIndex: true,
-                        useUnifiedTopology: true
-                        })
-        
-const connection = mongoose.connection
-connection.once('open', () => {
-    console.log('mongoDB database connection established successfully.')
-})
-
-app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+server.listen(PORT, () => console.log(`server is running on ${PORT}`))
 
 
 
