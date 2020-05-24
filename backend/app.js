63306cfc81c96ca91ba006f5b22d062272bebdd8
jjyoon7 <jay.yoon7@gmail.com>
2020-05-24 21:52:44 +0200

    comment out express.json passed as middleware to app.use -> to fix the 'EventEmitter memory leak detected.'

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -10,8 +10,8 @@ const userRoutes = require('./routes/user')
 const groceryRoutes = require('./routes/grocery')
 const uri = process.env.ATLAS_URI
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 app.use('/user', userRoutes)
 app.use('/grocery', groceryRoutes)
 
