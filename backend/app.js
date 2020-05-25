93864c3538cfeb3e90ee2f5e90da40ebdb354191
jjyoon7 <jay.yoon7@gmail.com>
2020-05-25 20:58:46 +0200

    add the error state to handle unexpected or falsy routes. Use next(), which when invoked, executes the middleware succeeding the current middleware.

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,22 @@ app.use(morgan('dev'))
 app.use('/user', userRoutes)
 app.use('/grocery', groceryRoutes)
 
+//if the routes does not match or exists, throw error
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status(404)
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.mesage
+        }
+    })
+})
+
 mongoose.connect(uri,  { 
                         useNewUrlParser: true,
                         useCreateIndex: true,
