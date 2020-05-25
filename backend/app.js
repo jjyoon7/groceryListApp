e898e1640a7411b6ebbd19e39009d3c1dc39fff1
jjyoon7 <jay.yoon7@gmail.com>
2020-05-25 21:04:23 +0200

    'error.status = 404', not 'error.status(404)'

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use('/grocery', groceryRoutes)
 //if the routes does not match or exists, throw error
 app.use((req, res, next) => {
     const error = new Error('Not found')
-    error.status(404)
+    error.status = 404
     next(error)
 })
 
