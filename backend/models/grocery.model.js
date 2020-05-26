e13761457bdc5275fda5ca01848cc4b3630a88e0
jjyoon7 <jay.yoon7@gmail.com>
2020-05-26 20:47:21 +0200

    update how Schema is initialized with mongoose

--- backend/models/grocery.model.js
+++ backend/models/grocery.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
-const grocerySchema = new Schema({
+const grocerySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {
         type: String,
